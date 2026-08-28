27466fc2ded114ff4bb187f98f62360696c10610
aronlontay <112824398+aronlontay@users.noreply.github.com>
2026-08-28 10:24:39 -0700

    halfway through lab save

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,26 @@
+// src/MyApp.jsx
+const characters = [
+    {
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ];
+  
+  function MyApp() {
+    return (
+        <div className="container">
+          <Table characterData={characters} />
+        </div>
+      );}
